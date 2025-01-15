35f90dd018456ad633d0a4a9b7bef52c942ff785
Ajay Devkate <125572042+Ajay-Devkate@users.noreply.github.com>
2025-01-15 16:36:53 +0530

    Update CategoryImg.js

--- swiggy-ui/src/components/CategoryImg.js
+++ swiggy-ui/src/components/CategoryImg.js
@@ -1,52 +1,52 @@
 const categoryImg = () => {
   const imgArr = [
     {
-      img: "./public/category/1.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/1.png",
     },
     {
-      img: "./public/category/2.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/2.png",
     },
     {
-      img: "./public/category/3.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/3.png",
     },
     {
-      img: "./public/category/4.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/4.png",
     },
     {
-      img: "./public/category/5.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/5.png",
     },
     {
-      img: "./public/category/6.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/6.png",
     },
     {
-      img: "./public/category/7.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/7.png",
     },
     {
-      img: "./public/category/8.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/8.png",
     },
     {
-      img: "./public/category/9.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/9.png",
     },
     {
-      img: "./public/category/10.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/10.png",
     },
     {
-      img: "./public/category/11.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/11.png",
     },
     {
-      img: "./public/category/12.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/12.png",
     },
     {
-      img: "./public/category/13.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/13.png",
     },
     {
-      img: "./public/category/14.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/14.png",
     },
     {
-      img: "./public/category/3.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/5.png",
     },
     {
-      img: "./public/category/6.png",
+      img: "https://raw.githubusercontent.com/Ajay-Devkate/swiggy-ui-clone/refs/heads/main/swiggy-ui/public/category/4.png",
     },
   ];
 
